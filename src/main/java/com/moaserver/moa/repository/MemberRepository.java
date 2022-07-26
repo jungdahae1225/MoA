f32da9145547196c14da56f68b6335e9d3586dfa
minji jung <haneul0804@naver.com>
2022-07-26 17:34:12 +0900

    [ADD] MyPage MVC

---
+++ src/main/java/com/moaserver/moa/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.moaserver.moa.repository;
+
+import com.moaserver.moa.entity.mypage.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface  MemberRepository extends JpaRepository<Member, Long> {
+
+
+    Optional<Member> findByNickname(String nickname);
+}
